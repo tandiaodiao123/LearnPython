579ecce49be37c645fe3f48dd1da8dce228c9cfe
datan <1473521733@qq.com>
2023-09-12 17:37:31 +0800

    创建了一个新的示例文件

--- test.py
+++ test.py
@@ -5,24 +5,22 @@ import json
 import time
 def test_text():
 
-    # Webhook地址
-    #url = "https://oapi.dingtalk.com/robot/send?access_token=" + " e8bXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXbfe2"
     url="https://oapi.dingtalk.com/robot/send?access_token="+"94f7f1da0e420066c0ceaca327ad7c7ff0f993d6d4b92141a8a6965dc962f690"
     print(url)
     HEADERS = {
         "Content-Type": "application/json ;charset=utf-8"
     }
     # 格式为：text
-    message = "姐姐 这是深夜故事汇"
+    message = "这是深夜故事汇"
     String_textMsg = {
         "msgtype": "text",
         "text": {"content": message + "\n"
-                 "我的姐姐呀" + "\n"
+                 "钉钉发送信息" + "\n"
                  "env" + "\n"
                  },
         "at": {
             "atMobiles": [
-               19983128202 #如果需要@某人，这里写他的手机号
+                #如果需要@某人，这里写他的手机号
             ],
             "isAtAll":   ""# 如果需要@所有人，这里写1
         }
@@ -33,6 +31,5 @@ def test_text():
     
     print(res.text)
 if __name__=="__main__":
-    for i in range(20):
         test_text()
 
